3494c1b4f6d895e02b7b663daba7c305902b1034
joelavero <joelavero@gmail.com>
2024-04-05 18:51:43 -0300

    fix add validation rules

--- src/helpers/validations.js
+++ src/helpers/validations.js
@@ -1,9 +1,17 @@
 const { body, param, oneOf } = require('express-validator');
 
 export const createTask = [
-  body('name').notEmpty().withMessage('name is required'),
+  body('name')
+    .notEmpty()
+    .withMessage('name is required')
+    .isLength({ max: 30 })
+    .withMessage('Name must be less than 30 characters'),
   body('description').notEmpty().withMessage('Description is required'),
-  body('author').notEmpty().withMessage('Author is required'),
+  body('author')
+    .notEmpty()
+    .withMessage('Author is required')
+    .isLength({ max: 30 })
+    .withMessage('Author name must be less than 30 characters'),
 ];
 
 export const validateId = [param('id').isInt().withMessage('ID must be a valid integer')];
@@ -11,11 +19,30 @@ export const validateId = [param('id').isInt().withMessage('ID must be a valid i
 export const updateTask = [
   oneOf(
     [
-      body('name').exists().isString(),
-      body('description').exists().isString(),
-      body('author').exists().isString(),
-      body('isComplete').exists().isBoolean(),
+      body('name').exists().notEmpty(),
+      body('description').exists().notEmpty(),
+      body('author').exists().notEmpty(),
+      body('isComplete').exists().notEmpty(),
     ],
     { msg: 'At least one of the following fields is required: name, description, author, isComplete' },
   ),
+  body('name')
+    .notEmpty()
+    .withMessage('name is required')
+    .isLength({ max: 30 })
+    .withMessage('Name must be less than 30 characters')
+    .optional(),
+  body('description').notEmpty().withMessage('Description is required').optional(),
+  body('author')
+    .notEmpty()
+    .withMessage('Author is required')
+    .isLength({ max: 30 })
+    .withMessage('Author name must be less than 30 characters')
+    .optional(),
+  body('isComplete')
+    .notEmpty()
+    .withMessage('isComplete is required')
+    .isBoolean()
+    .withMessage('isComplete must be a boolean')
+    .optional(),
 ];
--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,38 +1,3 @@
-/*
-import { validationResult } from 'express-validator';
-import { errorResponse } from '../helpers';
-
-const validate = (validations) => {
-  return async (req, res, next) => {
-    // eslint-disable-next-line no-restricted-syntax
-    for (const validation of validations) {
-      // eslint-disable-next-line no-await-in-loop
-      const result = await validation.run(req);
-      if (result.errors.length) break;
-    }
-
-    const errors = validationResult(req);
-
-    if (errors.isEmpty()) {
-      return next();
-    }
-
-    return errorResponse(
-      res,
-      ...errors.errors.map(({ msg }) => {
-        if (typeof msg === 'object' && msg.msg) {
-          return msg.msg;
-        }
-        return msg;
-      }),
-      400,
-    );
-  };
-};
-
-export default validate;
-*/
-
 import { validationResult } from 'express-validator';
 import createError from 'http-errors';
 import { BAD_REQUEST } from '../helpers/constants';
